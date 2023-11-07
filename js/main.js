3edadbca41b98967eed4736430a55a8450ed6bd0
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-11-08 02:56:43 +0500

    exam

---
+++ js/main.js
@@ -0,0 +1,30 @@
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
